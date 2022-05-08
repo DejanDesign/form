d74ef86fcdf13c04cef923ad2999e2581b5f3862
Dejan <64686921+DejanDesign@users.noreply.github.com>
2022-05-08 19:34:05 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const user = document.getElementById("username");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const errorDiv = document.getElementById("error");
+
+/*form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (
+    user.value === "" ||
+    (user.value == null && password.value === "") ||
+    password.value == null
+  ) {
+    messages.push("* please, enter username");
+    messages.push("* please, enter password");
+    user.style.borderColor = "red";
+    password.style.borderColor = "red";
+  }
+
+  if (user.value === "new_user" && password.value === "123456789") {
+    user.style.borderColor = "green";
+    password.style.borderColor = "green";
+    e.preventDefault();
+  }
+  if (!(user.value === "new_user")) {
+    user.style.borderColor = "red";
+    messages.push("* please, enter valid username");
+    e.preventDefault();
+  }
+  if (password.value !== "123456789") {
+    password.style.borderColor = "red";
+    messages.push("* please, enter valid password");
+    e.preventDefault();
+  }
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorDiv.innerText = messages.join(",");
+  }
+});*/
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (user.value === "" && password.value === "") {
+    messages.push("* please, enter username");
+    messages.push("* please, enter password");
+    user.style.borderColor = "red";
+    password.style.borderColor = "red";
+  }
+
+  if (user.value === "new_user" && password.value === "123456789") {
+    user.style.borderColor = "green";
+    password.style.borderColor = "green";
+    messages.push("");
+    e.preventDefault();
+  }
+  if (user.value !== "new_user" && user.value !== "") {
+    user.style.borderColor = "red";
+    messages.push("* please, enter valid username");
+    e.preventDefault();
+  }
+  if (password.value !== "123456789" && password.value !== "") {
+    password.style.borderColor = "red";
+    messages.push("* please, enter valid password");
+    e.preventDefault();
+  }
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorDiv.innerText = messages.join(",");
+  }
+});
